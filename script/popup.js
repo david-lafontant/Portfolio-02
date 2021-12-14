ca0347cb7da12b3ebac74ad4a360c163f8999331
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-14 15:35:22 -0600

    Make popup show and style its size and position

---
+++ script/popup.js
@@ -0,0 +1,31 @@
+const projectCards = document.querySelector('.card-pages');
+const projectBtn = document.querySelector('.myButton');
+
+projectBtn.addEventListener('click', showProject);
+
+function showProject() {
+    popup.style.top = "0";
+};
+
+let popup = document.createElement('div');
+popup.id = 'project-popup';
+popup.className = 'note'
+popup.innerHTML = '<p>Hello</p>';
+document.body.appendChild(popup);
+
+const style = document.createElement('style');
+style.innerHTML = `
+    .note {
+        background-color: yellow;
+        color:red;
+        width: 90vw;
+        height: 123vh;
+        position: fixed;
+        z-index: 100000;
+        top: 100;
+        margin: 5vw;
+        border-radius: 20px;
+        text-align: center;
+    }
+    `;
+document.head.appendChild(style);
