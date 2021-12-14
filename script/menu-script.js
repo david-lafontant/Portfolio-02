4ae3eaf981186076ca65d1ab7bd02647350196f4
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-14 14:47:15 -0600

    Change function names of how and hide

--- script/menu-script.js
+++ script/menu-script.js
@@ -3,18 +3,18 @@ const close = document.querySelector('.close');
 const open = document.querySelector('.open');
 const menuLinks = document.querySelectorAll('.menu-link');
 
-open.addEventListener('click', show);
-close.addEventListener('click', hide);
+open.addEventListener('click', showNavbar);
+close.addEventListener('click', hideNavbar);
 menuLinks.forEach(item => {
-  item.addEventListener('click', hide)
+  item.addEventListener('click', hideNavbar)
 });
 
-function show(){
+function showNavbar(){
   menu.style.display = "flex";
   menu.style.top = "0";
 }
 
-function hide(){
+function hideNavbar(){
   menu.style.top = "-100%";
 }
 
