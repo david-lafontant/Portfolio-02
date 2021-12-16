5236b54a7bb6e5f678b729efb5ae383b815a85f2
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2021-12-16 14:50:41 -0600

    Create event listener and func displayStoredData

--- script/store-data.js
+++ script/store-data.js
@@ -3,3 +3,9 @@ console.log("hello david");
 const name = document.querySelector('#name');
 const email = document.querySelector('#email');
 const msg = document.querySelector('#msg');
+
+const saveData = addEventListener('click', displayStoredData);
+
+function displayStoredData() {
+    console.log(name);
+}
