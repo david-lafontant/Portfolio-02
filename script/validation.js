1841913ad1ea016e163f575f12dc99240a5d8b6a
david-lafontant <lafontant.david@gmail.com>
2021-12-16 06:00:26 -0500

    Update validation script

--- script/validation.js
+++ script/validation.js
@@ -1,11 +1,13 @@
-// show a message with a type of the input
+// Show a message with a type of the input
 function showMessage(input, message, type) {
-    // get the small element and set the message
+    // Get the small element and set the message
     const msg = input.parentNode.querySelector("small");
+
     msg.innerText = message;
-    // update the class for the input
+    // Update the class for the input
     input.className = type ? "success" : "error";
-    return type;
+
+return type;
 }
 
 function showError(input, message) {
@@ -20,40 +22,44 @@ function hasValue(input, message) {
     if (input.value.trim() === "") {
         return showError(input, message);
     }
-    return showSuccess(input);
+
+return showSuccess(input);
 }
 
 function validateEmail(input, requiredMsg, invalidMsg) {
-    // check if the value is not empty
+    // Check if the value is not empty
     if (!hasValue(input, requiredMsg)) {
         return false;
     }
-    // validate email format
-    const emailRegex =
-        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // Validate email format
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
     const email = input.value.trim();
+
     if (!emailRegex.test(email)) {
         return showError(input, invalidMsg);
     }
-    return true;
+
+return true;
 }
 
-const form = document.querySelector("#signup");
+const form = document.querySelector("#form");
 
 const NAME_REQUIRED = "Please enter your name";
 const EMAIL_REQUIRED = "Please enter your email";
 const EMAIL_INVALID = "Please enter a correct email address format";
 
-form.addEventListener("submit", function (event) {
-    // stop form submission
+form.addEventListener("submit", event => {
+    // Stop form submission
     event.preventDefault();
 
-    // validate the form
-    let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-    let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
-    // if valid, submit the form.
+    // Validate the form
+    const nameValid = hasValue(form.elements.name, NAME_REQUIRED);
+    const emailValid = validateEmail(form.elements.email, EMAIL_REQUIRED, EMAIL_INVALID);
+    // If valid, submit the form.
+
     if (nameValid && emailValid) {
-        alert("Demo only. No form was posted.");
+        this submit()
+        alert("Invalid entries. Form not submitted");
     }
 });
