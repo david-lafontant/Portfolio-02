c8098c40d291d80e12fc608664334517dee8387e
david-lafontant <lafontant.david@gmail.com>
2021-12-18 02:16:51 -0500

    Modify popup.js - create modal

--- script/popup.js
+++ script/popup.js
@@ -1,43 +1,98 @@
-const projectCards = document.querySelector('.card-pages');
-const projectBtn = document.querySelector('.myButton');
+/* eslint-disable func-style */
+/* eslint-disable arrow-parens */
+const projectBtn = document.querySelectorAll('.myButton');
 
-projectBtn.addEventListener('click', showProject);
+const myHeadings = document.getElementsByClassName('cardH2');
 
-function showProject() {
-    popup.style.top = "0";
-};
+const headingContent = [];
 
+const webLanguages = [];
 
+const popup = [];
 
+const paragraph = `  <p class="cardP">
+Lorem Ipsum is simply dummy text of the printing and typesetting
+industry. Lorem Ipsum has been the industry's standard dummy text
+ever since the 1500s, when an unknown printer took a galley of type
+and scrambled it to make a type specimen book. It has survived not
+only five centuries, but also the leap into electronic typesetting,
+remaining essent
+</p>`;
 
+const closeSign = `<p><i class="fas fa-times closePopup"></i></p>`;
 
+const myCanopy = document.querySelectorAll('.cardUl1');
+const canopy = [];
+const web = document.getElementsByClassName('cardUl2');
 
+const myImages = document.getElementsByClassName('responsiveImg');
+const imageContent = [];
 
+for (let num = 0; num < myHeadings.length; num += 1){
+  const item = `<h2 class="cardH2">${myHeadings[num].textContent}</h2>`;
 
+  headingContent.push(item);
+}
 
+for (let num = 0; num < myImages.length; num += 1){
+  const item = `<img src=${myImages[num].src} alt="project image">`;
 
+  imageContent.push(item);
+}
 
+for (let num = 0; num < myCanopy.length; num += 1){
+  const item = `<ul class="cardUl1">${myCanopy[num].innerHTML}</ul>`;
 
-let popup = document.createElement('div');
-popup.id = 'project-popup';
-popup.className = 'note'
-popup.innerHTML = '<p>Hello</p>';
-document.body.appendChild(popup);
+  canopy.push(item);
+}
 
-const style = document.createElement('style');
-style.innerHTML = `
-    .note {
-        background-color: white;
-        color: black;
-        width: 90vw;
-        height: 123vh;
-        position: fixed;
-        z-index: 100000;
-        top: 100;
-        margin: 5vw;
-        border-radius: 20px;
-        text-align: center;
-        border: solid black
-    }
-    `;
-document.head.appendChild(style);
+for (let num = 0; num < web.length; num += 1){
+  const item = `<ul class="cardUl2">${web[num].innerHTML}</ul>`;
+
+  webLanguages.push(item);
+}
+
+for (let num = 0; num < myCanopy.length; num += 1){
+  const item = {
+"theHead": headingContent[num],
+"theCanopy": canopy[num],
+"theImage": imageContent[num]
+  };
+
+popup.push(item);
+}
+
+// eslint-disable-next-line no-unused-vars
+const popupTemplate = (num) => `<section class="cardPages">
+<section class="card-details">
+  ${closeSign}
+  ${popup[num].theHead}
+  ${popup[num].theCanopy}
+</section>
+<section class="snapshot">
+${popup[num].theImage}
+</section>
+<section class="card-details">
+${paragraph}
+${webLanguages[num]}
+  <div class="flexButton">
+    <button class="myButton">See project</button>
+    <button class="myButton">See project</button>
+  </div>
+</section>
+</section>
+<script src="script/popup.js"></script>
+`;
+function closePopup(){
+  const closed = document.querySelector('.closePopup');
+  closed.addEventListener('click', async () => {
+    window.location.reload();
+  });
+}
+// eslint-disable-next-line no-plusplus
+for (let i = 0; i < projectBtn.length; i++) {
+  projectBtn[i].addEventListener('click', () => {
+    document.body.innerHTML = popupTemplate(i);
+    closePopup();
+  });
+}
